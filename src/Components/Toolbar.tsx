3830b33331bfb1d2bdbc725b09f6e62802d2ee2d
Usman Abdur Rehman <saidskit@outlook.com>
2024-09-02 00:59:05 +0500

    Initialzed Tutorial Code

--- src/Components/Toolbar.tsx
+++ src/Components/Toolbar.tsx
@@ -1,51 +1,15 @@
-import { Box, ButtonGroup, Flex, IconButton, Select } from "@chakra-ui/react";
+import { ButtonGroup, Flex, IconButton, Select } from "@chakra-ui/react";
 import { css } from "@emotion/css";
 import {
   HEADINGS,
-  RichTextAction,
   TEXT_BLOCK_OPTIONS,
   TEXT_FORMAT_OPTIONS,
 } from "../constants";
 import { Divider } from "./Divider";
-import { useSlate } from "slate-react";
-import { ElementKey, MarkKey } from "../types";
-import { isBlockActive, isMarkActive, toggleBlock, toggleMark } from "../utils";
 
 interface ToolbarProps {}
 
 export default function Toolbar({}: ToolbarProps) {
-  const editor = useSlate();
-
-  const getMarkSelectionProps = (id: RichTextAction) => {
-    if (isMarkActive(editor, id as MarkKey)) {
-      return {
-        colorScheme: "blue",
-        variant: "solid",
-      };
-    }
-  };
-
-  const getBlockSelectionProps = (id: RichTextAction) => {
-    if (isBlockActive(editor, id as ElementKey)) {
-      return {
-        colorScheme: "blue",
-        variant: "solid",
-      };
-    }
-  };
-
-  const onMarkClick = (format: RichTextAction) => {
-    toggleMark(editor, format as MarkKey);
-  };
-
-  const onBlockClick = (format: RichTextAction) => {
-    toggleBlock(editor, format as ElementKey);
-  };
-
-  const onHeadingClick = (format: string) => {
-    toggleBlock(editor, format as ElementKey);
-  };
-
   return (
     <Flex gap={4}>
       <ButtonGroup
@@ -58,39 +22,17 @@ export default function Toolbar({}: ToolbarProps) {
           alignItems: "center",
         })}
       >
-        <Select
-          size="md"
-          mr={2}
-          placeholder="paragraph"
-          onChange={(e) => onHeadingClick(e.target.value)}
-        >
+        <Select size="md" mr={2} placeholder="paragraph" onChange={(e) => {}}>
           {HEADINGS.map((heading) => (
             <option value={heading}>{heading}</option>
           ))}
         </Select>
         {TEXT_FORMAT_OPTIONS.map(({ id, label, icon, fontSize }) => (
-          <IconButton
-            aria-label={label}
-            icon={icon}
-            fontSize={fontSize}
-            onMouseDown={(e) => {
-              e.preventDefault();
-              onMarkClick(id);
-            }}
-            {...getMarkSelectionProps(id)}
-          />
+          <IconButton aria-label={label} icon={icon} fontSize={fontSize} />
         ))}
         <Divider />
         {TEXT_BLOCK_OPTIONS.map(({ id, label, icon }) => (
-          <IconButton
-            aria-label={label}
-            icon={icon}
-            onMouseDown={(e) => {
-              e.preventDefault();
-              onBlockClick(id);
-            }}
-            {...getBlockSelectionProps(id)}
-          />
+          <IconButton aria-label={label} icon={icon} />
         ))}
       </ButtonGroup>
     </Flex>
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,41 +1,10 @@
-import { Box, Button } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import { Descendant } from "slate";
-import { useData, useUpdateData } from "./api";
+import { Box } from "@chakra-ui/react";
 import { RichTextEditor } from "./RichTextEditor";
 
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
-
 export default function Form() {
-  const [value, setValue] = useState<Descendant[]>();
-  const { mutateAsync: saveText, isPending } = useUpdateData();
-  const { data, isSuccess } = useData();
-
-  useEffect(() => {
-    setValue(data || []);
-  }, [data]);
-
-  const onSave = () => {
-    saveText(JSON.stringify(value));
-  };
-
   return (
     <Box p={2}>
-      <RichTextEditor
-        placeholder="Enter Post"
-        name="post"
-        // initialValue={isSuccess ? data || [] : undefined}
-        initialValue={initialValue}
-        onChange={(newValue) => setValue(newValue)}
-      />
-      <Button colorScheme="whatsapp" size="md" mt={2} onClick={onSave}>
-        {isPending ? "Saving..." : "Save"}
-      </Button>
+      <RichTextEditor />
     </Box>
   );
 }
